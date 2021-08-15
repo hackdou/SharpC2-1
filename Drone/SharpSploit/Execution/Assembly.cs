41a46d6efb1d526e6dcad49af58369fef541b569
Rasta Mouse <rasta@rastamouse.me>
2021-08-15 12:48:24 +0100

    appdomain

--- Drone/SharpSploit/Execution/Assembly.cs
+++ Drone/SharpSploit/Execution/Assembly.cs
@@ -1,4 +1,5 @@
-﻿using Reflect = System.Reflection;
+﻿using System;
+using Reflect = System.Reflection;
 
 namespace Drone.SharpSploit.Execution
 {
@@ -7,9 +8,21 @@ namespace Drone.SharpSploit.Execution
         public static void Execute(byte[] assemblyBytes, string[] args = null)
         {
             if (args == null) args = new string[] { };
+
+            var domain = AppDomain.CreateDomain(Guid.NewGuid().ToShortGuid(), null, null, null, false);
+            var proxy = (ShadowRunnerProxy)domain.CreateInstanceAndUnwrap(typeof(ShadowRunnerProxy).Assembly.FullName, typeof(ShadowRunnerProxy).FullName);
+            proxy.LoadAssembly(assemblyBytes, args);
             
-            var asm = Reflect.Assembly.Load(assemblyBytes);
-            asm.EntryPoint.Invoke(null, new object[] {args});
+            AppDomain.Unload(domain);
+        }
+    }
+
+    public class ShadowRunnerProxy : MarshalByRefObject
+    {
+        public void LoadAssembly(byte[] bytes, string[] args)
+        {
+            var asm = Reflect.Assembly.Load(bytes);
+            asm.EntryPoint.Invoke(null, new object[] { args });
         }
     }
 }
